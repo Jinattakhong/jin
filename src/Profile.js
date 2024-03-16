2997183e86d8723e54d0d8ba4ca9265dd2b3f942
unknown <jinattacatcat@gmail.com>
2024-03-16 10:28:17 +0700

    first commit

--- src/Profile.js
+++ src/Profile.js
@@ -5,7 +5,7 @@ function Profile() {
     return (
         <div className="App">
             <h1>Profile</h1>
-            <img src="http://mgt2.pnu.ac.th/tinata/revenge%20dress.png" alt="My image" width={200}
+            <img src="http://mgt2.pnu.ac.th/tinata/6460704009/revenge%20dress.png" alt="My image" width={200}
   height={300} />
               <h1>นางสาวจิณัฐตา คงคุณ</h1>
               <h1>รหัสนักศึกษา6460704009 </h1>
